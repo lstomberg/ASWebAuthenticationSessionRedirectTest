8faac82f20d76dc0825aad402df0f59eec14d598
Lucas Stomberg <lstomber@epic.com>
2020-05-19 16:36:24 -0500

    Initial Commit

---
+++ ASWebAuthenticationSessionRedirectTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ASWebAuthenticationSessionRedirectTest
+//
+//  Created by Lucas Stomberg on 5/19/20.
+//  Copyright © 2020 Epic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
